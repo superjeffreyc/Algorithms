b7dd859f8525118f8aa63331e5d7a43510e921d0
Jeffrey Chan <superjeffreyc@yahoo.com>
2015-12-17 22:55:44 -0500

    Completed checkmate

--- Binghamton ACM/checkmate.c
+++ Binghamton ACM/checkmate.c
@@ -6,15 +6,25 @@
 #include <math.h>
 #include <stdlib.h>
 
+void attackHV(int i, int j, int **matrix){
+    // TODO
+}
+
+void attackDiagonal() {
+    // TODO
+}
+
 int main() {
 
-    char lines[5], i, *p;
-    int num_lines;
+    char lines[5], i, j, k, *p;
+    int num_lines, iKing, jKing, iAdd, jAdd, itemp, jtemp;
 
     fgets(lines, 5, stdin);
     num_lines = atoi(lines);
 
     char input[num_lines][num_lines+5];
+    int matrix[num_lines][num_lines];
+
     for (i = 0; i < num_lines; i++){
         fgets(input[i], num_lines+5, stdin);
 
@@ -23,13 +33,194 @@ int main() {
         }
     }
 
-    // TODO:
-    // Assuming King is already in check
-    // For each of the empty tiles surrounding the King, check if the King would be in check if moved to that tile
-    // For each tile surrounding the King with an enemy piece, check if that piece is protected
+    // Initialize array to 0
+    for (i = 0; i < num_lines; i++){
+    	for (j = 0; j < num_lines; j++){
+            matrix[i][j] = 0;
 
-    // Use a matrix with binary values and bitwise OR to determine all attackable areas by black.
+            // Look for King
+            if (input[i][j] == 'K'){
+                iKing = i;
+                jKing = j;
+            }
+        }
+    }
 
+    // Create a matrix of all attackable areas
+    for (i = 0; i < num_lines; i++){
+    	for (j = 0; j < num_lines; j++){
+    		if (input[i][j] == 'R'){
+                // Vertical attacks
+                for (itemp = 0; itemp < num_lines; itemp++){
+                    if (i != itemp){
+                        matrix[itemp][j] = 1;
+                    }
+                }
+                // Horizontal attacks
+                for (jtemp = 0; jtemp < num_lines; jtemp++){
+                    if (j != jtemp){
+                        matrix[i][jtemp] = 1;
+                    }
+                }
+            }
+            else if (input[i][j] == 'Q'){
+                // Vertical attacks
+                for (itemp = 0; itemp < num_lines; itemp++){
+                    if (i != itemp){
+                        matrix[itemp][j] = 1;
+                    }
+                }
+                // Horizontal attacks
+                for (jtemp = 0; jtemp < num_lines; jtemp++){
+                    if (j != jtemp){
+                        matrix[i][jtemp] = 1;
+                    }
+                }
+
+                // Diagonal attacks
+                for (k = 0; k < 4; k++){
+                    itemp = i;
+                    jtemp = j;
+
+                    switch(k){
+
+                        case 0:
+                            // Diagonal attack top left
+                            iAdd = -1;
+                            jAdd = -1;
+                            break;
+                        case 1:
+                            // Diagonal attack top right
+                            iAdd = -1;
+                            jAdd = 1;
+                            break;
+                        case 2:
+                            // Diagonal attack bottom left
+                            iAdd = 1;
+                            jAdd = -1;
+                            break;
+                        case 3:
+                            // Diagonal attack bottom right
+                            iAdd = 1;
+                            jAdd = 1;
+                            break;
+                    }
+                    while (itemp >= 0 && itemp < num_lines && jtemp >= 0 && jtemp < num_lines){
+                        if (itemp != i && jtemp != j){
+                            matrix[itemp][jtemp] = 1;
+                        }
+                        itemp += iAdd;
+                        jtemp += jAdd;
+                    }
+                }
+
+            }
+            else if (input[i][j] == 'N'){
+
+                // 8 possible knight attacks
+                for (k = 0; k < 8; k++){
+                    itemp = i;
+                    jtemp = j;
+
+                    switch(k){
+
+                        case 0:
+                            iAdd = -2;
+                            jAdd = -1;
+                            break;
+                        case 1:
+                            iAdd = -2;
+                            jAdd = 1;
+                            break;
+                        case 2:
+                            iAdd = -1;
+                            jAdd = 2;
+                            break;
+                        case 3:
+                            iAdd = 1;
+                            jAdd = 2;
+                            break;
+                        case 4:
+                            iAdd = 2;
+                            jAdd = -1;
+                            break;
+                        case 5:
+                            iAdd = 2;
+                            jAdd = 1;
+                            break;
+                        case 6:
+                            iAdd = -1;
+                            jAdd = -2;
+                            break;
+                        case 7:
+                            iAdd = 1;
+                            jAdd = -2;
+                            break;
+                    }
+
+                    itemp += iAdd;
+                    jtemp += jAdd;
+
+                    if (itemp >= 0 && itemp < num_lines && jtemp >= 0 && jtemp < num_lines){
+                        matrix[itemp][jtemp] = 1;
+                    }
+                }
+            }
+            else if (input[i][j] == 'B'){
+
+                // Diagonal attacks
+                for (k = 0; k < 4; k++){
+                    itemp = i;
+                    jtemp = j;
+
+                    switch(k){
+
+                        case 0:
+                            // Diagonal attack top left
+                            iAdd = -1;
+                            jAdd = -1;
+                            break;
+                        case 1:
+                            // Diagonal attack top right
+                            iAdd = -1;
+                            jAdd = 1;
+                            break;
+                        case 2:
+                            // Diagonal attack bottom left
+                            iAdd = 1;
+                            jAdd = -1;
+                            break;
+                        case 3:
+                            // Diagonal attack bottom right
+                            iAdd = 1;
+                            jAdd = 1;
+                            break;
+                    }
+                    while (itemp >= 0 && itemp < num_lines && jtemp >= 0 && jtemp < num_lines){
+                        if (itemp != i && jtemp != j){
+                            matrix[itemp][jtemp] = 1;
+                        }
+                        itemp += iAdd;
+                        jtemp += jAdd;
+                    }
+                }
+            }
+    	}
+    }
+
+    // Check surrounding tiles to see if King is in checkmate
+    char *message = "checkmate";
+    for (i = iKing - 1; i <= iKing + 1; i++){
+        for (j = jKing - 1; j <= jKing + 1; j++){
+            if (i >= 0 && i < num_lines && j >= 0 && j < num_lines){
+                if (matrix[i][j] == 0){
+                    message = "safe";
+                }
+            }
+        }
+    }
+
+    printf("%s", message);
 
     return 0;
 }
